474c52a6b7eb49bc2e4bf9a1968064913ac07a3a
amaity-digi <116334087+amaity-digi@users.noreply.github.com>
2023-09-24 13:07:28 +0530

    created folder structure, Implemented esLint

---
+++ src/features/cart/CartOverView.jsx
@@ -0,0 +1,14 @@
+
+function CartOverView() {
+  return (
+    <div>
+        <p>
+            <span>23 Pizzas</span>
+            <span>$7</span>
+        </p>
+        <a href="/">Open Cart &rarr;</a>
+    </div>
+  )
+}
+
+export default CartOverView
